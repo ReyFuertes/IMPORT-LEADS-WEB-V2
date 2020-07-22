867918ff1b2980014d2b34903c186afb016a6695
RFuertes <reynelfuertes@gmail.com>
2020-07-22 17:41:45 +0800

    change env url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   svgPath: 'assets/svg/',
   imgPath: 'assets/image/',
-  apiUrl: 'http://localhost:3000/api/v1/',
-  apiImagePath: 'http://localhost:3000/api/v1/contracts/image/'
+  apiUrl: 'http://178.128.111.112:3000/api/v1/',
+  apiImagePath: 'http://178.128.111.112:3000/api/v1/contracts/image/'
 };

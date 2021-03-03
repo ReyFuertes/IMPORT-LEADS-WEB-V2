71f7bec87b13ad3d84c6a7d086928c67e1704083
Rfuertes <reynelfuertes@gmail.com>
2021-03-03 20:17:11 +0800

    add image viewer in comment and change dev url

--- src/app/modules/inspections/components/inspection-report-comments/inspection-report-comments.component.ts
+++ src/app/modules/inspections/components/inspection-report-comments/inspection-report-comments.component.ts
@@ -6,6 +6,8 @@ import { environment } from 'src/environments/environment';
 import { inspectionOkCommentsReport } from '../../store/actions/inspection-report.action';
 import { getInspectionOkCommentsReportSelector } from '../../store/selectors/inspection-report.selector';
 import * as _ from 'lodash';
+import { MatDialog } from '@angular/material/dialog';
+import { ImageViewerDialogComponent } from 'src/app/modules/dialogs/components/image-viewer-dialog/image-viewer-dialog.component';
 @Component({
   selector: 'il-inspection-report-comments',
   templateUrl: './inspection-report-comments.component.html',
@@ -19,7 +21,7 @@ export class InspectionReportCommentsComponent implements OnInit {
   public apiImagePath: string = environment.apiImagePath;
   public imgPath: string = environment.imgPath;
 
-  constructor(private route: ActivatedRoute, private store: Store<AppState>) {
+  constructor(private dialog: MatDialog, private route: ActivatedRoute, private store: Store<AppState>) {
 
     const saved_checklist_id = this.route.snapshot.paramMap.get('id') || null;
     if (saved_checklist_id) {
@@ -33,6 +35,12 @@ export class InspectionReportCommentsComponent implements OnInit {
         if (res) 
           this.dataSource = res;
       });
+  }
+
+  public viewImage(image: string): void {
+    const imageViewDialog = this.dialog.open(ImageViewerDialogComponent, { data: { image } });
+    imageViewDialog.afterClosed()
+      .subscribe(result => { });
   }
 
   public get getProductCommentCount(): any {

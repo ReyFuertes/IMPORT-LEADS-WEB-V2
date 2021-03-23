d1593776a13e4df22bc51b6680c614b3a6876a0c
Rfuertes <reynelfuertes@gmail.com>
2021-03-23 18:26:57 +0800

    downloads path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: false,
   svgPath: 'assets/svg/',
   imgPath: 'assets/image/',
-  apiUrl: 'http://localhost:3001/iaad/api/v1/',
-  apiImagePath: 'http://localhost:3001/iaad/api/v1/images/',
+  apiUrl: 'http://localhost:3000/iaad/api/v1/',
+  apiImagePath: 'http://localhost:3000/iaad/api/v1/images/',
+  apiDownloadsPath: 'http://localhost:3000/iaad/api/v1/downloads/',
   localEncKey: '5qH4u2spHRrYA2TmDakwfD9HuJ7K6Fkv7YzDsj92'
 };
